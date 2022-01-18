25cfbef2ff2a52ce1fc478499f457b440566f5cf
Nikola Samardzic <nsamar@mit.edu>
2022-01-18 15:52:16 -0500

    [build] Add debug flags

--- HEAAN/lib/src/subdir.mk
+++ HEAAN/lib/src/subdir.mk
@@ -17,7 +17,7 @@ CPP_SRCS += \
 ../src/SerializationUtils.cpp \
 ../src/StringUtils.cpp \
 ../src/TestScheme.cpp \
-../src/TimeUtils.cpp 
+../src/TimeUtils.cpp
 
 OBJS += \
 ./src/BootContext.o \
@@ -33,7 +33,7 @@ OBJS += \
 ./src/SerializationUtils.o \
 ./src/StringUtils.o \
 ./src/TestScheme.o \
-./src/TimeUtils.o 
+./src/TimeUtils.o
 
 CPP_DEPS += \
 ./src/BootContext.d \
@@ -49,14 +49,14 @@ CPP_DEPS += \
 ./src/SerializationUtils.d \
 ./src/StringUtils.d \
 ./src/TestScheme.d \
-./src/TimeUtils.d 
+./src/TimeUtils.d
 
 
 # Each subdirectory must supply rules for building sources it contributes
 src/%.o: ../src/%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -I/usr/local/include -O3 -c -std=c++11 -pthread -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	clang++-10 -I/usr/local/include -O3 -g -ggdb -fno-omit-frame-pointer -c -std=c++11 -pthread -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
